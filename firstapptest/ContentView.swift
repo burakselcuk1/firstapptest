368410873fa64d162584f05cce657bfbec74fda6
burakselcuk <design.burak1@gmail.com>
2024-12-22 17:53:12 +0300

    Initial Commit

---
+++ firstapptest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firstapptest
+//
+//  Created by Burak Selçuk on 22.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firstapptestTests/firstapptestTests.swift
@@ -0,0 +1,17 @@
+//
+//  firstapptestTests.swift
+//  firstapptestTests
+//
+//  Created by Burak Selçuk on 22.12.2024.
+//
+
+import Testing
+@testable import firstapptest
+
+struct firstapptestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
