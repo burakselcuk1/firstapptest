2eaa795f46ec92adca515b874e982d49a8ebf62b
burakselcuk <design.burak1@gmail.com>
2024-12-22 17:57:48 +0300

    first ommit

--- firstapptest/ContentView.swift
+++ firstapptest/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Count \(count)")
+            
+            Button {
+                count += 1 // Butona basıldığında count artar
+            } label: {
+                Text("Artır")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
         }
         .padding()
     }
